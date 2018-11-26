d2381f05083a534c20553cd30fbc8c5639316990
rocky122 <38912058+rocky122@users.noreply.github.com>
2018-11-26 21:09:54 +0530

    added spinner and loader to simulate async call

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,48 +1,48 @@
 <template>
   <div>
-  <div v-if="!orderDone">
-    <keep-alive>
-      <component 
-      ref="currentStep"
-      :is="currentStep"
-      @update="processStep"
-      :isLastStep="isLastStep"
-      :wizardProgress="wizardProgress"
-      :wizardData="form">
-    </component>
-    </keep-alive>
-    <!-- <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/> -->
-    <!-- <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
+    <div v-if="!orderDone && wizardProgress" v-show="asyncState !== 'pending'">
+      <keep-alive>
+        <component
+          ref="currentStep"
+          :is="currentStep"
+          @update="processStep"
+          :isLastStep="isLastStep"
+          :wizardProgress="wizardProgress"
+          :wizardData="form"
+        ></component>
+      </keep-alive>
+      <!-- <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/> -->
+      <!-- <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
     <FormAddress v-if="currentStepNumber === 3" :wizardData="form" @update="processStep"/>
-    <FormReviewOrder v-if="currentStepNumber === 4" :wizardData="form" @update="processStep"/> -->
+      <FormReviewOrder v-if="currentStepNumber === 4" :wizardData="form" @update="processStep"/>-->
+      <div class="progress-bar">
+        <div :style="`width: ${progress}%;`"></div>
+      </div>
 
-    <div class="progress-bar">
-      <div :style="`width: ${progress}%;`"></div>
+      <!-- Actions -->
+      <div class="buttons">
+        <button @click="goBack" v-if="currentStepNumber > 1" class="btn-outlined">Back</button>
+        <button
+          @click="nextButtonAction"
+          class="btn"
+          :disabled="!moveAllowed"
+        >{{isLastStep ? 'Confirm Order' : 'Next'}}</button>
+      </div>
+      <!-- <pre><code>{{form}}</code></pre> -->
     </div>
-
-    <!-- Actions -->
-    <div class="buttons">
-      <button
-        @click="goBack"
-        v-if="currentStepNumber > 1"
-        class="btn-outlined"
-      >Back
-      </button>
-      <button
-        @click="nextButtonAction"
-        class="btn"
-        :disabled="!moveAllowed"
-      >{{isLastStep ? 'Confirm Order' : 'Next'}}</button>
+    <div v-else>
+      <h1 class="title">Thank you !!</h1>
+      <h2 class="subtitle">We look forward to shipping your first box!</h2>
+      <p class="text-center">
+        <a href="https://github.com/instillhubdev" target="_blank" class="btn">Go to InstillHubDev</a>
+      </p>
     </div>
-    <!-- <pre><code>{{form}}</code></pre> -->
-  </div>
-  <div v-else>
-    <h1 class="title">Thank you !!</h1>
-    <h2 class="subtitle">We look forward to shipping your first box!</h2>
-    <p class="text-center">
-      <a href="https://github.com/instillhubdev" target="_blank" class="btn">Go to InstillHubDev</a>
-    </p>
-  </div>
+    <div class="loading-wrapper" v-if="asyncState === 'pending'">
+      <div class="loader">
+        <img src="/spinner.svg" alt>
+        <p>Please wait we are hitting our servers....</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -65,6 +65,7 @@ export default {
       currentStepNumber: 1,
       moveAllowed: false,
       orderDone: false,
+      asyncState : null,
       form: {
         plan: null,
         email: null,
@@ -111,8 +112,10 @@ export default {
     async submitOrder() {
       //api call to server
       try {
+        this.asyncState = 'pending';
         await postFormToDB(this.form);
         console.log(`form submitted ${this.form}`);
+        this.asyncState = 'success';
         this.currentStepNumber++;
         this.orderDone = true;
       } catch (err) {
